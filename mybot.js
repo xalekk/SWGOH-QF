b4485c6d05cb24c8e264b05d990ce503465cfbc1
xalekk <xalekk@gmail.com>
2018-05-28 20:31:45 -0400

    correction !sithhp1

--- mybot.js
+++ mybot.js
@@ -503,6 +503,7 @@ client.on("message", async message => {
     	// makes the bot say something and delete the message. As an example, it's open to anyone to use.
     	// To get the "message" itself we join the `args` back into a string with spaces:
   	const dayHour = args.join(" ");
+	sithRole = msg.channel.server.roles.get("name","SITH")
 	let sithRole = message.guild.roles.get("448198255915040775");
     	// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     	message.delete().catch(O_o=>{});
