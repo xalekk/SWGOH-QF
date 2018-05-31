925391a47912d62c1a90d8a702f6eba34c608e34
xalekk <xalekk@gmail.com>
2018-05-30 21:14:45 -0400

    correction !sithhp1

--- mybot.js
+++ mybot.js
@@ -502,8 +502,9 @@ client.on("message", async message => {
     if(command === "sithhp2") {
     	// makes the bot say something and delete the message. As an example, it's open to anyone to use.
     	// To get the "message" itself we join the `args` back into a string with spaces:
-  	const dayHour = args.join(" ");
 	let role = guild.roles.get("451549948366946304");
+  	const dayHour = args.join(" ");
+	
 	   	// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     	message.delete().catch(O_o=>{});
     	// And we get the bot to say the thing:
