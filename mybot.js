6ee48c8d52d047cb0462125fd59de7adcb99dfcf
xalekk <xalekk@gmail.com>
2018-05-30 21:06:16 -0400

    correction \n

--- mybot.js
+++ mybot.js
@@ -503,11 +503,11 @@ client.on("message", async message => {
     	// makes the bot say something and delete the message. As an example, it's open to anyone to use.
     	// To get the "message" itself we join the `args` back into a string with spaces:
   	const dayHour = args.join(" ");
-	//let sithRole = msg.channel.server.roles.get("name","SITH");
+	let role = guild.roles.get('451549948366946304');
 	   	// Then we delete the command message (sneaky, right?). The catch just ignores the error with a cute smiley thing.
     	message.delete().catch(O_o=>{});
     	// And we get the bot to say the thing:
-
+	
 	const embed = new Discord.RichEmbed()
     	//.setTitle("__**RAID SITH HEROIC**__")
 	.setDescription(`:point_right: Phase 2 :point_left:`)
@@ -516,7 +516,7 @@ client.on("message", async message => {
 	.setAuthor(message.member.displayName, message.author.avatarURL)
 	.setColor(0xa20410)
 	.setThumbnail("https://swgoh.gg/static/img/assets/tex.charui_sionraid.png")
-    message.channel.send(`__**:boom: SITH HEROIC:boom:**__ <@&451548364190777355>`,{embed});
+    message.channel.send(`__**:boom: SITH HEROIC:boom:**__ ${role}`,{embed});
 
   }
   
